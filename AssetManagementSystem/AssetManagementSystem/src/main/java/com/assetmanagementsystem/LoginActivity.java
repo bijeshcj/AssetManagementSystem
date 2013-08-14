33ae15e12b64e96be767c2aef31b4e3bf2bf6e00
bijeshcj <bijesh.jayaprakash@gmail.com>
2013-08-14 12:06:36 +0530

    AssetManagementSystem initialized

---
+++ AssetManagementSystem/AssetManagementSystem/src/main/java/com/assetmanagementsystem/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.assetmanagementsystem;
+
+import android.os.Bundle;
+
+/**
+ * Created by Bijesh C J on 14/8/13.
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
