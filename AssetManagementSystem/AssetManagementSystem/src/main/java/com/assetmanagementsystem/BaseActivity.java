51aacff6a65a1de22887782fb1570d8b174fffbd
bijeshcj <bijesh.jayaprakash@gmail.com>
2013-09-02 21:55:40 +0530

    AMS-C-1: tasks updated, centered the login creds

--- AssetManagementSystem/AssetManagementSystem/src/main/java/com/assetmanagementsystem/BaseActivity.java
+++ AssetManagementSystem/AssetManagementSystem/src/main/java/com/assetmanagementsystem/BaseActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class BaseActivity extends Activity {
-
+    protected static String TAG = "AssetMangSystem";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- AssetManagementSystem/AssetManagementSystem/src/main/java/com/assetmanagementsystem/LoginActivity.java
+++ AssetManagementSystem/AssetManagementSystem/src/main/java/com/assetmanagementsystem/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.assetmanagementsystem;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by Bijesh C J on 14/8/13.
@@ -10,5 +12,8 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+    }
+    public void executeLogin(View view){
+        Log.d(TAG, "Login button clicked....");
     }
 }
